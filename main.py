60c0a016e39d028668c43ea80511c9d8690aaee2
Караев Сергей <karaevsv@perfectsoft.ru>
2024-02-14 17:23:51 +0300

    Список профессий

--- main.py
+++ main.py
@@ -11,7 +11,14 @@ def index():
 
 @app.route('/training/<prof>')
 def profession(prof):
-    return render_template('index.html', prof=prof)
+    return render_template('index.html',title='Заготовка', prof=prof)
+
+@app.route('/list_prof/<style>')
+def show_list(style):
+    lst = ['инженер-исследователь', 'пилот', 'строитель', 'экзобиолог', 'врач', 'инженер по терраформированию',
+           'климатолог', 'специалист по радиационной защите', 'астрогеолог', 'гляциолог', 'инженер жизнеобеспечения',
+           'метеоролог', 'оператор марсохода', 'киберинженер', 'штурман', 'пилот дронов']
+    return render_template('list.html', style=style, lst=lst)
 
 
 if __name__ == '__main__':
