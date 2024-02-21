95949a32825cd75b0dd1132253ac5fd2cff27ea3
Караев Сергей <karaevsv@perfectsoft.ru>
2024-02-21 18:02:26 +0300

    add captain

--- main.py
+++ main.py
@@ -1,32 +1,66 @@
-from flask import Flask, url_for, request, render_template
+from flask import Flask
+from data import db_session
+from data.users import User
+
+from data.jobs import Jobs
+import datetime
+import sqlalchemy
+from data.db_session import SqlAlchemyBase
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
-@app.route('/')
-@app.route('/index')
-def index():
-    return render_template('base.html', title='Заготовка')
 
 
-@app.route('/training/<prof>')
-def profession(prof):
-    return render_template('index.html',title='Заготовка', prof=prof)
 
-@app.route('/list_prof/<style>')
-def show_list(style):
-    lst = ['инженер-исследователь', 'пилот', 'строитель', 'экзобиолог', 'врач', 'инженер по терраформированию',
-           'климатолог', 'специалист по радиационной защите', 'астрогеолог', 'гляциолог', 'инженер жизнеобеспечения',
-           'метеоролог', 'оператор марсохода', 'киберинженер', 'штурман', 'пилот дронов']
-    return render_template('list.html', style=style, lst=lst)
+def main():
 
-@app.route('/answer')
-@app.route('/auto_answer')
-def give_answer():
-    anketa = {'title': 'Анкета', 'surname': 'Watny', 'name': 'Mark', 'education': 'выше среднего',
-              'profession': 'штурман марсохода', 'sex': 'male', 'motivation': 'Всегда мечтал застрять на Марсе!',
-              'ready': 'True'}
-    return render_template('auto_answer.html', anketa=anketa, title=anketa['title'])
+    db_session.global_init("db/mars_explorer.db")
+    captain = User()
+    captain.surname = 'Scott'
+    captain.name = "Ridley"
+    captain.age = 21
+    captain.position = 'captain'
+    captain.speciality = 'research engineer'
+    captain.address = 'module_1'
+    captain.email = "scott_chief@mars.org"
+
+    user1 = User()
+    user1.surname = 'Ivanov'
+    user1.name = "Pavel"
+    user1.age = 27
+    user1.position = 'main engineer'
+    user1.speciality = 'spaceship engineer'
+    user1.address = 'module_2'
+    user1.email = "email1@email.ru"
+
+    user2 = User()
+    user2.surname = 'Smit'
+    user2.name = "Jack"
+    user2.age = 19
+    user2.position = 'cleaner'
+    user2.speciality = 'poet'
+    user2.address = 'module_3'
+    user2.email = "email2@email.ru"
+
+    user3 = User()
+    user3.surname = 'Green'
+    user3.name = "Anna"
+    user3.age = 30
+    user3.position = 'doctor'
+    user3.speciality = 'nurse'
+    user3.address = 'medical office'
+    user3.email = "email3@email.ru"
+
+    db_sess = db_session.create_session()
+    db_sess.add(captain)
+    db_sess.add(user1)
+    db_sess.add(user2)
+    db_sess.add(user3)
+    db_sess.commit()
+    app.run()
+
 
 if __name__ == '__main__':
-    app.run(port=8080, host='127.0.0.1')
+    main()
