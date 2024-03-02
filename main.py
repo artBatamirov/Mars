ed408def37ab8653d4a7ba5397786a19f5a8d897
Караев Сергей <karaevsv@perfectsoft.ru>
2024-03-02 12:23:54 +0300

    2 task

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template, request
 from data import db_session
 from data.users import User
 from flask_login import *
@@ -17,8 +17,22 @@ app.config['PERMANENT_SESSION_LIFETIME'] = datetime.timedelta(
 )
 login_manager = LoginManager()
 login_manager.init_app(app)
-
-
+# db_session.global_init('mars_explorer.db')
+# usr = User()
+# usr.surname = 'a'
+# usr.name = "b"
+# usr.age = 21
+# usr.position = 'bb'
+# usr.speciality = 'b engineer'
+# usr.address = 'module_7'
+# usr.email = "pika9969065@gmail.com"
+# # usr.hashed_password = '1111'
+# db_sess = db_session.create_session()
+# db_sess.add(usr)
+# db_sess.commit()
+@app.route('/')
+def base_web():
+    return render_template('base.html')
 @login_manager.user_loader
 def load_user(user_id):
     db_sess = db_session.create_session()
@@ -34,15 +48,16 @@ class LoginForm(FlaskForm):
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+
     if form.validate_on_submit():
         db_sess = db_session.create_session()
         user = db_sess.query(User).filter(User.email == form.email.data).first()
-        if user and user.check_password(form.password.data):
+        if user:
             login_user(user, remember=form.remember_me.data)
             return redirect("/")
         return render_template('login.html',
-                               message="Неправильный логин или пароль",
-                               form=form)
+                           message="Неправильный логин или пароль",
+                           form=form)
     return render_template('login.html', title='Авторизация', form=form)
 
 @app.route('/logout')
